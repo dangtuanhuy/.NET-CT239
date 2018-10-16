09f8cac8b9b0122c5e8221337ba2592b40fb7401
Unknown <phongb1505902@student.ctu.edu.vn>
2018-10-16 15:04:37 +0700

    code user + db

--- MyWatch/MyWatchWatch/Controllers/HomeController.cs
+++ MyWatch/MyWatchWatch/Controllers/HomeController.cs
@@ -3,15 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyWatchWatch.Models;
 
 namespace MyWatchWatch.Controllers
 {
     public class HomeController : Controller
     {
+        MyWatchWatchEntities db = new MyWatchWatchEntities();
         public ActionResult Index()
         {
+            var lstProduct1 = db.Products.Where(n => n.ProductStatus == true && n.CategoryId == 2);
+            ViewBag.ListProduct1 = lstProduct1;
+
+            var lstProduct2 = db.Products.Where(n => n.ProductStatus == true && n.CategoryId == 3);
+            ViewBag.ListProduct2 = lstProduct2;
+
+            var lstProduct3 = db.Products.Where(n => n.ProductStatus == true && n.CategoryId == 4);
+            ViewBag.ListProduct3 = lstProduct3;
+           
             return View();
         }
+        public ActionResult Details(int id = 0, string name = "")
+        {
+            var viewModel = db.Products.Find(id);
+            if (id <= 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var productDetails = db.Products
+                .Include("ImgProducts")
+                .SingleOrDefault(item => item.ProductId == id);
+
+            if (productDetails == null)
+            {
+                return View("404");
+            }
+
+            var exceptedEntities = db.Products.Where(result => result.ProductId == viewModel.ProductId);
+            ViewBag.RelatedProduct =
+                db.Products.Where(item => item.CategoryId == viewModel.CategoryId).Except(exceptedEntities).ToList();
+            return View(productDetails);
+        }
+
+
+
         public ActionResult _slider()
         {
             return PartialView();
@@ -28,5 +64,6 @@ namespace MyWatchWatch.Controllers
         {
             return PartialView();
         }
+
     }
 }
---
+++ MyWatch/MyWatchWatch/Models/ItemGioHang.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyWatchWatch.Models;
+namespace MyWatchWatch.Models
+{
+    public class ItemGioHang
+    {
+        public Product product { set; get; }
+        public ImgProduct ImgProduct { set; get; }
+        public int ProductQtyUser { set; get; }
+        public int ProductId { set; get; }
+        public decimal ProductSold { get; set; }
+        public List<ImgProduct> ImgPro { get; set; }
+        public String ProductName { get; set; }
+        public Decimal ThanhTien { get; set; }
+
+        public ItemGioHang(int ProductId)
+        {
+            using (MyWatchWatchEntities db = new MyWatchWatchEntities())
+            {
+                Product pro = db.Products.Include("ImgProducts").Single(x => x.ProductId == ProductId);
+                this.ProductId = ProductId;
+                this.ProductQtyUser = 1;
+                this.ProductName = pro.ProductName;
+                this.ProductSold = pro.ProductSold.Value;
+                this.ThanhTien = pro.ProductSold.Value * this.ProductQtyUser;
+                this.ImgPro = pro.ImgProducts.ToList();
+
+            }
+        }
+    }
+}
