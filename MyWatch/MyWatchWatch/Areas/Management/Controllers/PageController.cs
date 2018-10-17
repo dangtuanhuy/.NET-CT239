4b1931b726b26f7eb6877d829657ab540e8d2eb9
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-10-17 13:01:14 +0700

    Count

--- MyWatch/MyWatchWatch/Areas/Management/Controllers/PageController.cs
+++ MyWatch/MyWatchWatch/Areas/Management/Controllers/PageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyWatchWatch.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,29 @@ namespace MyWatchWatch.Areas.Management.Controllers
 {
     public class PageController : Controller
     {
+        private MyWatchWatchEntities db = new MyWatchWatchEntities();
         // GET: Management/Page
         public ActionResult Page()
         {
             return View();
+        }
+        public ActionResult _countProducts()
+        {
+            var countProducts = db.Products.Count();
+            ViewBag.countProducts = countProducts;
+            return PartialView();
+        }
+        public ActionResult _statusPromotin()
+        {
+            var statusPromotin = (from d in db.Promotions where d.PromotionStatus == true select d).Count();
+            ViewBag.statusPromotin = statusPromotin;
+            return PartialView();
+        }
+        public ActionResult _countCustomer()
+        {
+            var countCus = db.Customers.Count();
+            ViewBag.countCus = countCus;
+            return PartialView();
         }
     }
 }
